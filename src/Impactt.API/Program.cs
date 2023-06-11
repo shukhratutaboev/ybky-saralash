ebbc6bb5b6b2b8075601c2b6641baddd539826bd
Shukhrat Utaboev <shuhratutaboyev2597@gmail.com>
2023-06-10 21:52:59 -0700

    Update Program.cs swagger

--- src/Impactt.API/Program.cs
+++ src/Impactt.API/Program.cs
@@ -41,11 +41,8 @@ using (var scope = serviceProvider.CreateScope())
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
