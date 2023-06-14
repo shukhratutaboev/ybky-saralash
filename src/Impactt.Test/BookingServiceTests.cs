c680662751851e05d301fcc81fe9ef7de1a6d480
Shukhrat Utaboev <shuhratutaboyev2597@gmail.com>
2023-06-14 14:08:49 +0500

    Update BookingServiceTests.cs

--- src/Impactt.Test/BookingServiceTests.cs
+++ src/Impactt.Test/BookingServiceTests.cs
@@ -48,7 +48,7 @@ public class BookingServiceTests
         {
             Page = 1,
             PageSize = 3,
-            Type = "team-error"
+            Type = "team"
         };
 
         var result = await _bookingService.GetRoomsAsync(query);
