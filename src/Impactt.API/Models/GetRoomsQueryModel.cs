08900fd999f46d815747c1eb3af4abb4236329eb
Shukhrat Utaboev <shuhratutaboyev2597@gmail.com>
2023-06-24 20:42:34 -0700

    Update GetRoomsQueryModel.cs

--- src/Impactt.API/Models/GetRoomsQueryModel.cs
+++ src/Impactt.API/Models/GetRoomsQueryModel.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Impactt.API.Models;
 
 public class GetRoomsQueryModel
@@ -5,16 +7,21 @@ public class GetRoomsQueryModel
     private int _page = 1;
     private int _pageSize = 10;
 
+    [FromQuery(Name = "page")]
     public int Page {
         get { return _page; }
         set { _page = value > 0 ? value : 1; }
     }
 
+    [FromQuery(Name = "page_size")]
     public int PageSize {
         get { return _pageSize; }
         set { _pageSize = value > 0 ? value : 10; }
     }
     
+    [FromQuery(Name = "search")]
     public string Search { get; set; }
+    
+    [FromQuery(Name = "type")]
     public string Type { get; set; }
 }
